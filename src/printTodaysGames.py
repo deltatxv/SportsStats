0b2ec57618da10de39a29ac304a319e5fb8d6e01
Aaron Van Doren <vandoren96+1@gmail.com>
2024-01-30 22:36:49 -0800

    - add scores to output if game already started - outline idea of class to hold data for each game instead of remembering array indices

--- src/printTodaysGames.py
+++ src/printTodaysGames.py
@@ -8,7 +8,12 @@ def printGame(game):
     home_team = game[0]
     away_team = game[1]
     time = game[2]
-    print(f"{Fore.YELLOW}{time}:\t\t{OUTPUT_COLORS['AWAY']}{away_team} {OUTPUT_COLORS['@']}@ {OUTPUT_COLORS['HOME']}{home_team}")
+    
+    if "EST" not in time: # means already started (i.e. Q1, Final, etc.)
+        home_score = game[3]
+        away_score = game[4]
+        
+    print(f"{Fore.YELLOW}{time}:\t\t{OUTPUT_COLORS['AWAY']}{away_score} {OUTPUT_COLORS['@']}- {OUTPUT_COLORS['HOME']}{home_score}\t\t{OUTPUT_COLORS['AWAY']}{away_team} {OUTPUT_COLORS['@']}@ {OUTPUT_COLORS['HOME']}{home_team}")
     
 def printGames(games):
     for game in games:
@@ -16,8 +21,8 @@ def printGames(games):
 
 def printTodaysGames():
     data = get_todays_games_json_from_url(TODAYS_GAMES_URL)
-    games = create_todays_games_from_json(data)
-    print()
+    games = get_games_from_json(data)
+    print(data)
     print(f"{OUTPUT_COLORS['HEADER']}There are {len(games)} games scheduled for today:\n")
     printGames(games)
     print()
--- src/utils/tools.py
+++ src/utils/tools.py
@@ -14,14 +14,37 @@ def get_todays_games_json_from_url(url):
     raw_data = requests.get(url, headers=games_header)
     json = raw_data.json()
     return json.get('gs').get('g')
+'''
+{
+    - team abbrev
+    - team city
+    - team name
+    - full name = name + city
+    - q1 score
+    - q2 score
+    - q3 score
+    - q4 score
+    - total score
+}
+'''
 
-def create_todays_games_from_json(input_list):
+def get_games_from_json(input_list):
     games = []
     for game in input_list:
         home = game.get('h')
         away = game.get('v')
         time = game.get('stt')
+        
         home_team = home.get('tc') + ' ' + home.get('tn')
         away_team = away.get('tc') + ' ' + away.get('tn')
-        games.append([home_team, away_team, time])
+        
+        home_score = home.get('s')
+        away_score = away.get('s')
+        
+        games.append([home_team, away_team, time, home_score, away_score])
     return games
+
+# def get_streaming_link_for_game(game):
+#     # TODO: make mapping for correct team names "Golden State Warriors" doesnt work"
+#     home_team = game[0]
+#     return f"https://methstreams.com/nba-streams/{home_team}/"
