5b75d5b35906135c4ea8d93d5aca6c2ae4753006
Aaron Van Doren <vandoren96+1@gmail.com>
2024-01-30 16:57:42 -0800

    Formatting improvements

--- src/printTodaysGames.py
+++ src/printTodaysGames.py
@@ -8,7 +8,7 @@ def printGame(game):
     home_team = game[0]
     away_team = game[1]
     time = game[2]
-    print(f"{Fore.YELLOW}{time}: {OUTPUT_COLORS['AWAY']}{away_team} {OUTPUT_COLORS['@']}@ {OUTPUT_COLORS['HOME']}{home_team}")
+    print(f"{Fore.YELLOW}{time}:\t\t{OUTPUT_COLORS['AWAY']}{away_team} {OUTPUT_COLORS['@']}@ {OUTPUT_COLORS['HOME']}{home_team}")
     
 def printGames(games):
     for game in games:
@@ -17,7 +17,7 @@ def printGames(games):
 def printTodaysGames():
     data = get_todays_games_json_from_url(TODAYS_GAMES_URL)
     games = create_todays_games_from_json(data)
-    print("\n")
+    print()
     print(f"{OUTPUT_COLORS['HEADER']}There are {len(games)} games scheduled for today:\n")
     printGames(games)
-    print("\n")
+    print()
